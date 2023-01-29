d6bf17ab3934f4f0b3ca8d7bd944ac8c2daacf5d
Gavin D'souza <gavin18d@gmail.com>
2023-01-29 15:54:43 +0530

    feat: Link MariaDB queries to tables
    Came across (Frappe)? bug where URL isn't correctly set in queries Table at this
    point

--- toolbox/toolbox/doctype/mariadb_table/mariadb_table.py
+++ toolbox/toolbox/doctype/mariadb_table/mariadb_table.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2023, Gavin D'souza and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
@@ -12,3 +12,22 @@ class MariaDBTable(Document):
     # 2. Show table indexes / index size (MiB)
     # 3. Show table health status / fragmentation
     # 5. Show table row count / require partitioning?
+
+    def load_from_db(self):
+        super().load_from_db()
+        self.load_queries()
+
+    def load_queries(self):
+        queries = frappe.get_all(
+            "MariaDB Query",
+            filters={"table": self.name},
+            fields=["*"],
+            order_by="occurence desc",
+        )
+        self.set("queries", queries)
+        for qry in self.queries:
+            qry.doctype = "MariaDB Query"
+
+    @property
+    def num_queries(self):
+        return len(self.queries)
