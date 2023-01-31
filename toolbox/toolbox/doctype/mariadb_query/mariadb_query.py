1a63c1ebc277b6e0cbdbead8f1b1e9355cd6599a
Gavin D'souza <gavin18d@gmail.com>
2023-01-31 16:06:35 +0530

    refactor: MariaDB Query
    * Use Long Text to store queries because of large char limit
    * Maintain explain data in mariadb query explain child table
    * Maintain tables order in read only summary field
    * Maintain query table id from explain

--- toolbox/toolbox/doctype/mariadb_query/mariadb_query.py
+++ toolbox/toolbox/doctype/mariadb_query/mariadb_query.py
@@ -3,6 +3,7 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe.utils import cint
 
 
 class MariaDBQuery(Document):
@@ -10,40 +11,30 @@ class MariaDBQuery(Document):
         self.set_tables_summary()
 
     def set_tables_summary(self):
-        tables_id = {x.table for x in self.query_explain}
+        seen = set()
+        tables_id = [
+            x.table for x in self.query_explain if not (x.table in seen or seen.add(x.table))
+        ]
         tables = frappe.get_all(
-            "MariaDB Table", filters={"name": ("in", tables_id)}, pluck="_table_name"
+            "MariaDB Table", filters={"name": ("in", tables_id)}, fields=["name", "_table_name"]
         )
-        self.tables = frappe.as_json(tables)
+        tables.sort(key=lambda x: tables_id.index(x["name"]))
+        self.tables = frappe.as_json([x._table_name for x in tables], indent=0)
 
-    def apply_explain(self, explain, table_id):
-        from frappe.utils import cint
+    def apply_explain(self, explain: dict, table_id: str):
+        explain_row = {
+            "table": table_id,
+            "id": explain["id"],
+            "type": explain["type"],
+            "possible_keys": explain["possible_keys"],
+            "key": explain["key"],
+            "key_len": cint(explain["key_len"]),
+            "ref": explain["ref"],
+            "rows": cint(explain["rows"]),
+            "extra": explain["Extra"],
+        }
 
-        if self.get(
-            "query_explain",
-            {
-                "table": table_id,
-                "type": explain["type"],
-                "possible_keys": explain["possible_keys"],
-                "key": explain["key"],
-                "key_len": cint(explain["key_len"]),
-                "ref": explain["ref"],
-                "rows": cint(explain["rows"]),
-                "extra": explain["Extra"],
-            },
-        ):
+        if self.get("query_explain", explain_row):
             return
 
-        self.append(
-            "query_explain",
-            {
-                "table": table_id,
-                "type": explain["type"],
-                "possible_keys": explain["possible_keys"],
-                "key": explain["key"],
-                "key_len": explain["key_len"],
-                "ref": explain["ref"],
-                "rows": explain["rows"],
-                "extra": explain["Extra"],
-            },
-        )
+        self.append("query_explain", explain_row)
