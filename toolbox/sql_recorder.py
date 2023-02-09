5c3de6d68a702965ad9545a847611ba51b8becaf
Gavin D'souza <gavin18d@gmail.com>
2023-02-09 16:33:23 +0530

    feat: SQL Recorder
    Similar to Frappe's recorder, but short & sweet, no extra overheads
    during runtime

---
+++ toolbox/sql_recorder.py
@@ -0,0 +1,86 @@
+import inspect
+import time
+from contextlib import suppress
+from re import compile
+
+import frappe
+
+TRACEBACK_PATH_PATTERN = compile(".*/apps/")
+TOOLBOX_RECORDER_FLAG = "toolbox_sql_profiler_enabled"
+TOOLBOX_RECORDER_DATA = "toolbox_sql_profiler_records"
+
+
+def export_data() -> list[list[dict[str, str | float | list[dict]]]]:
+    return list(frappe.cache().hgetall(TOOLBOX_RECORDER_DATA).values())
+
+
+def delete_data():
+    frappe.cache().delete_value(TOOLBOX_RECORDER_DATA)
+
+
+def sql(*args, **kwargs):
+    start_time = time.monotonic()
+    result = frappe.local.db_sql(*args, **kwargs)
+    end_time = time.monotonic()
+
+    frappe.local.toolbox_recorder.register(
+        {
+            "query": frappe.db.last_query,
+            "args": args,
+            "kwargs": kwargs,
+            "stack": list(get_current_stack_frames()),
+            "time": start_time,
+            "duration": float(f"{(end_time - start_time) * 1000:.3f}"),
+        }
+    )
+
+    return result
+
+
+def _patch():
+    frappe.local.db_sql = frappe.db.sql
+    frappe.db.sql = sql
+
+
+def _unpatch():
+    frappe.db.sql = frappe.local.db_sql
+
+
+def before_request():
+    if frappe.cache().get_value(TOOLBOX_RECORDER_FLAG):
+        frappe.local.toolbox_recorder = SQLRecorder()
+        _patch()
+
+
+def after_request():
+    if frappe.cache().get_value(TOOLBOX_RECORDER_FLAG):
+        frappe.local.toolbox_recorder.dump()
+        _unpatch()
+
+
+def get_current_stack_frames():
+    with suppress(Exception):
+        current = inspect.currentframe()
+        frames = inspect.getouterframes(current, context=10)
+        for frame, filename, lineno, function, context, index in list(reversed(frames))[:-2]:
+            if "/apps/" in filename:
+                yield {
+                    "filename": TRACEBACK_PATH_PATTERN.sub("", filename),
+                    "lineno": lineno,
+                    "function": function,
+                }
+
+
+class SQLRecorder:
+    def __init__(self):
+        self.uuid = frappe.generate_hash(length=10)
+        self.queries = []
+
+    def register(self, data):
+        self.queries.append(data)
+
+    def dump(self):
+        if not self.queries:
+            return
+        self.queries, dump = [], self.queries[:]
+        frappe.cache().hset(TOOLBOX_RECORDER_DATA, self.uuid, dump)
